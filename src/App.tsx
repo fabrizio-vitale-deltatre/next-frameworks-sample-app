699a405a6c7154952ce98c04232bcbc1a793feb3
favitale <fabrizio.vitale@alten.it>
2022-10-05 09:07:12 +0200

    feat: add solid app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,65 @@
-import type { Component } from "solid-js";
+import {
+  createResource,
+  JSX,
+  Suspense,
+  ErrorBoundary,
+  For,
+} from "solid-js";
+import { fetchWikipediaOpenSearch } from "./api/wikipedia";
 
-const App: Component = () => {
-  return <div />;
-};
+function getSearchTerm() {
+  const params = new URLSearchParams(window.location.search);
+  return params.get("q");
+}
 
-export default App;
+function Articles() {
+  const [searchResults] = createResource(
+    getSearchTerm,
+    fetchWikipediaOpenSearch
+  );
+
+  return (
+    <ul class="wikipedia-links">
+      <For each={searchResults()?.[1]} fallback={<div>0 results :(</div>}>
+        {(title, idx) => (
+          <li>
+            <a
+              target="_blank"
+              class="paper-btn btn-primary-outline"
+              rel="noopener noreferrer"
+              href={searchResults()?.[3][idx()]}
+            >
+              {title}
+            </a>
+          </li>
+        )}
+      </For>
+    </ul>
+  );
+}
+
+export function App(): JSX.Element {
+  const query = getSearchTerm() || "";
+
+  const [searchResults] = createResource(
+    getSearchTerm,
+    fetchWikipediaOpenSearch
+  );
+
+  return (
+    <article>
+      <h1>Wikipedia search: {query}</h1>
+      <ErrorBoundary
+        fallback={
+          <div class="row flex-spaces">
+            <div class="alert alert-danger">{String(searchResults.error)}</div>
+          </div>
+        }
+      >
+        <Suspense fallback={<div class="alert alert-primary">Loading...</div>}>
+          <Articles />
+        </Suspense>
+      </ErrorBoundary>
+    </article>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@
 import { render } from "solid-js/web";
 import "./";
 import "./index.css";
-import App from "./App";
+import { App } from "./App";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
