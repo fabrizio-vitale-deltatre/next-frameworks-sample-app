6a1a9c2095cfc2d3e4a6f58f00373ea92083c980
Fabrizio Vitale <fabrizio.vitale@deltatre.com>
2023-07-30 10:09:32 +0200

    fix: app not working on lg webos 4.9.7

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
+import legacy from '@vitejs/plugin-legacy'
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), legacy({ targets: ['Chrome 50']})],
   build: {
     outDir: "dist/vite-solid",
   },
