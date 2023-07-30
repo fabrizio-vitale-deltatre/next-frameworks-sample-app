eb960a04da11cdc1cd0838dc6285bbbcd6902b1b
Fabrizio Vitale <fabrizio.vitale@deltatre.com>
2023-07-30 10:09:32 +0200

    fix: app not working on lg webos 4.9.7

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from '@vitejs/plugin-legacy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), legacy({ targets: ['Chrome 50']})],
   build: {
     outDir: "dist/vite-react",
   },
