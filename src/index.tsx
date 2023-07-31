8cfd008ac6691fb3bf5c69182b97bcf1c39bfea8
Fabrizio Vitale <fabrizio.vitale@deltatre.com>
2023-07-31 13:18:09 +0200

    fix: bundle evaluated twice on simulator

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,17 @@ import { render } from "solid-js/web";
 import "./index.css";
 import { App } from "./App";
 
-render(() => <App />, document.getElementById("root") as HTMLElement);
+// On the Chrome version hosted on the simulator,
+// Vite incorrectly runs the legacy bundle & the modern bundle.
+const hasEvalutatedAppBundleKey = "hasEvalutatedAppBundle";
+
+if (!(window as any)[hasEvalutatedAppBundleKey]) {
+  Object.defineProperty(window, hasEvalutatedAppBundleKey, {
+    value: true,
+    enumerable: false,
+    writable: false,
+    configurable: false,
+  });
+
+  render(() => <App />, document.getElementById("root") as HTMLElement);
+}
